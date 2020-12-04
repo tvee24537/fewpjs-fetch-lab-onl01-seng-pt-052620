6f1f65fa5c4eefd6a1bf0952594518bc40309a1e
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-12-04 23:11:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function fetchBooks() {
-  fetch('https://anapioficeandfire.com/api/books')
+  return fetch('https://anapioficeandfire.com/api/books')
     .then(resp => resp.json())
-    .then(json => console.log(json));
+    .then(json => renderBooks(json))
 }
 
 function renderBooks(books) {
